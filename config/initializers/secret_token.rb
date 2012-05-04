426358b33e3981726d87b1622d10c2e8ed2cf9ca
Luke Ledet <luke@lootbox.org>
2012-05-03 22:03:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deej::Application.config.secret_token = 'ba0ae32b1582a5a1da0c66618bdebc222c42586159c9a5921081ebc206249a639730d4791428207049c4b4c499a53924172bc50b9d5d7b780cc4dbe32886b5f4'
