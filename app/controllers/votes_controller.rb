549272bf3137778bf7130cf32df75a956d8a3771
Luke Ledet <luke@lootbox.org>
2012-05-04 15:58:33 -0500

    Let the user know his/her vote was counted immediately

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,6 @@
 class VotesController < ApplicationController
   def new
-    song = Song.find params[:song_id]
-
-    @vote = song.votes.create ip: request.remote_ip
+    @song = Song.find params[:song_id]
+    @vote = @song.votes.create ip: request.remote_ip
   end
 end
