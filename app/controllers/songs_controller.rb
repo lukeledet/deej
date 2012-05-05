da51600074ac910afd2fd7551a2076413241ce29
Luke Ledet <luke@lootbox.org>
2012-05-05 14:55:44 -0500

    Using table-condensed for the song list to show more songs per page

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.order('artist, album').page(params[:page]).per(15)
+    @songs = Song.order('artist, album').page(params[:page]).per(20)
 
     if params[:query]
       @songs = @songs.search params[:query]
